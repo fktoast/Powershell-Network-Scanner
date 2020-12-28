e22d39c190ed5c814f341b35c21140ca36ed3a33
Frank Koch Jr <fktoast@gmail.com>
2020-12-28 12:05:12 -0600

    Update AddSubnet.ps1

--- AddSubnet.ps1
+++ AddSubnet.ps1
@@ -1,6 +1,6 @@
 ﻿1..254 | % {
     Request-SQL.Automation.Record -Table IP_Addresses -Payload @{
-        IP_Address = "192.168.7.$_";
+        IP_Address = "192.168.1.$_";
         IP_Type="";
     }
 }
